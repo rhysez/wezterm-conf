643ef12babb4be25d6594f5786f871e383817423
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2025-07-09 21:18:57 +0100

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,9 @@
-return {
-    color_scheme = 'Gruvbox dark, pale (base16)',
-    tab_bar_at_bottom = true,
-    use_fancy_tab_bar = false,
-    window_decorations = "RESIZE"
+local config = {
+        color_scheme = "Gruvbox dark, pale (base16)",
+        tab_bar_at_bottom = true,
+        use_fancy_tab_bar = false,
+        window_decorations = "RESIZE",
+        window_background_opacity = 0.8,
 }
+
+return config
